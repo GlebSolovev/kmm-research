e116532579403f8875398da3242ec8b9d9dc0618
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2023-08-25 21:47:13 +0200

    [decompiler] add tests for generated psi text by builtin decompiler

---
+++ analysis/decompiled/decompiler-to-psi/tests/org/jetbrains/kotlin/analysis/decompiler/psi/BuiltinDecompilerToTextTest.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.analysis.decompiler.psi
+
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.impl.jar.CoreJarFileSystem
+import com.intellij.psi.PsiManager
+import org.jetbrains.kotlin.analysis.decompiler.stub.files.AbstractDecompiledClassTest
+import org.jetbrains.kotlin.idea.KotlinLanguage.INSTANCE
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.test.KotlinTestUtils
+import java.nio.file.Paths
+
+class BuiltinDecompilerToTextTest : AbstractDecompiledClassTest() {
+    fun testBuiltinDecompilationToText() {
+        val decompiledBuiltInKtFiles = loadBuiltIns()
+        for (file in decompiledBuiltInKtFiles) {
+            val resultFileName = file.name + DECOMPILED_TEXT_EXTENSION
+            val expectedFile = Paths.get(TEST_DATA_PATH).resolve(resultFileName)
+            KotlinTestUtils.assertEqualsToFile(expectedFile, file.text)
+        }
+    }
+
+    private fun loadBuiltIns(): Collection<KtFile> {
+        val psiManager = PsiManager.getInstance(project)
+        val builtInDecompiler = KotlinBuiltInDecompiler()
+        return BuiltInsVirtualFileProvider.getInstance().getBuiltInVirtualFiles().map { virtualFile ->
+            createKtFileStub(psiManager, builtInDecompiler, virtualFile)
+        }
+    }
+
+    private fun createKtFileStub(
+        psiManager: PsiManager,
+        builtInDecompiler: KotlinBuiltInDecompiler,
+        virtualFile: VirtualFile,
+    ): KtFile {
+        val fileViewProvider = builtInDecompiler.createFileViewProvider(virtualFile, psiManager, physical = true)
+        val psiFile = fileViewProvider.getPsi(INSTANCE)
+        return psiFile as? KtFile
+            ?: error("Expected some KtFile for $virtualFile but $psiFile found")
+    }
+
+    override fun setUp() {
+        super.setUp()
+        val applicationEnvironment = environment.projectEnvironment.environment
+        val application = applicationEnvironment.application
+        if (application.getService(BuiltInsVirtualFileProvider::class.java) == null) {
+            application.registerService(
+                BuiltInsVirtualFileProvider::class.java,
+                BuiltInsVirtualFileProviderCliImpl(applicationEnvironment.jarFileSystem as CoreJarFileSystem)
+            )
+        }
+    }
+
+    companion object {
+        private const val TEST_DATA_PATH = "analysis/decompiled/decompiler-to-psi/testData/builtins"
+        private const val DECOMPILED_TEXT_EXTENSION = ".decompiled.text.kt"
+    }
+}
